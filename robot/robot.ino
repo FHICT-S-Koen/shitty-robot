7a76c0982db59b155a5b90a89fc0f1eccc0b400a
Ekhorn <koen@kschellingerhout.nl>
2024-03-06 11:51:04 +0100

    wip

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,23 +1,104 @@
-//Arduino PWM Speed Control：
-int E1 = 5; // M1-
-int M1 = 4; // M1+
-int E2 = 6; // M2-
-int M2 = 7; // M2+
+#include <SPI.h>
+
+//Add the SdFat Libraries
+#include <SdFat.h>
+#include <SdFatUtil.h>
+
+//and the MP3 Shield Library
+#include <SFEMP3Shield.h>
+
+/**
+ * \brief Object instancing the SdFat library.
+ *
+ * principal object for handling all SdCard functions.
+ */
+SdFat sd;
+
+/**
+ * \brief Object instancing the SFEMP3Shield library.
+ *
+ * principal object for handling all the attributes, members and functions for the library.
+ */
+SFEMP3Shield MP3player;
+
+/**
+ * Arduino PWM DC motor speed control:
+ * E1 = M1-
+ * M1 = M1+
+ * E2 = M2-
+ * M2 = M2+
+ */
+int E1 = 5;
+int M1 = 4;
+int E2 = 6;
+int M2 = 7;
 int buttonPin = 2;
 
 void setup()
 {
-  //   // initialize the LED pin as an output:
-  // pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
   // initialize the pushbutton pin as an input:
   // pinMode(buttonPin, INPUT);
   pinMode(M1, OUTPUT);
   pinMode(M2, OUTPUT);
+
+  //Initialize the SdCard.
+  if(!sd.begin(SD_SEL, SPI_FULL_SPEED)) sd.initErrorHalt();
+  // depending upon your SdCard environment, SPI_HAVE_SPEED may work better.
+  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
+
+  uint8_t result = MP3player.begin();
+  MP3player.setVolume(0,0);
+  if(result != 0) {
+    Serial.print(F("Error code: "));
+    Serial.print(result);
+    Serial.println(F(" when trying to start MP3 player"));
+    if( result == 6 ) {
+      Serial.println(F("Warning: patch file not found, skipping.")); // can be removed for space, if needed.
+      Serial.println(F("Use the \"d\" command to verify SdCard can be read")); // can be removed for space, if needed.
+    }
+  }
 }
 
 void loop()
 {
-  shoot();
+  bool start = true;
+
+  // Start game
+  if (!start) {
+    // Zijn jullie ready?
+    playTrack(1);
+    delay(10000);
+    return;
+  }
+
+  // Het spel gaat starten!! 3,2,1 GO! 
+  // playTrack(9);
+
+  // Go Go Go! 
+  // Sneller! 
+  // Kan je ze allemaal vinden?
+
+  time
+  while() 
+  playTrack(random(3, 6));
+
+  // delay(1000);
+
+  // Tijdens het spel 
+
+
+
+
+
+
+  // Eind spel 
+
+  // Alle balletjes zijn geschoten! 
+  // Hebben jullie alles gevonden? 
+  // Hervul me om een nieuw spel te starten. 
+
+  shoot(5000);
   // digitalWrite(M1,HIGH);
   // digitalWrite(M2, HIGH);
   // analogWrite(E1, 255);   //PWM Speed Control
@@ -40,14 +121,49 @@ void loop()
   // }
 }
 
-void shoot() 
+void shoot(int ms) 
 {
-  digitalWrite(M1,HIGH);
+  digitalWrite(M1, HIGH);
   digitalWrite(M2, HIGH);
   analogWrite(E1, 255);   //PWM Speed Control
   analogWrite(E2, 255);   //PWM Speed Control
+  delay(ms);
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, LOW);
 }
 
+void playTrack(uint8_t track)
+{
+  uint8_t result = MP3player.playTrack(track);
+
+  if(result != 0) {
+    Serial.print(F("Error code: "));
+    Serial.print(result);
+    Serial.println(F(" when trying to play track"));
+  } else {
+    Serial.print(F("Playing: "));
+    Serial.println(track);
+
+
+    // MP3player.stopTrack();
+
+
+    // //we can get track info by using the following functions and arguments
+    // //the functions will extract the requested information, and put it in the array we pass in
+    // MP3player.trackArtist((char*)&artist);
+    // MP3player.trackAlbum((char*)&album);
+
+    // //print out the arrays of track information
+    // Serial.write((byte*)&title, 30);
+    // Serial.println();
+    // Serial.print(F("by:  "));
+    // Serial.write((byte*)&artist, 30);
+    // Serial.println();
+    // Serial.print(F("Album:  "));
+    // Serial.write((byte*)&album, 30);
+    // Serial.println();
+  }
+}
 
 // /**set control port**/
 // // const int E1Pin = 10;
