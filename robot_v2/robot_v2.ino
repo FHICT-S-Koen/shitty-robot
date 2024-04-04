0a37b28f5962d63cf89bb105e816cf93d7bcfdcf
Ekhorn <koen@kschellingerhout.nl>
2024-04-04 12:26:22 +0200

    fine-tuning stuff

--- robot_v2/robot_v2.ino
+++ robot_v2/robot_v2.ino
@@ -1,8 +1,4 @@
 const int buttonPin = A2;
-int buttonState;            // the current reading from the input pin
-int lastButtonState = LOW;  // Previous state of the button
-unsigned long lastDebounceTime = 0;  // Time of the last button state change
-unsigned long debounceDelay = 30;  // Debounce time in milliseconds
 
 // Motors
 #include <Servo.h>
@@ -15,19 +11,8 @@ AF_DCMotor dc_motor_2(2);
 // DC motors PWM speed control
 const int SPEED = 200;
 
-// MP3/SD
-#include <SPI.h>
-#include <SdFat.h>
-#include <SdFatUtil.h>
-#include <SFEMP3Shield.h>
-
-SdFat sd;
-SFEMP3Shield MP3player;
-// MP3 Player volume 0=max, 255=lowest (off)
-const uint8_t volume = 0;
-
 void setup() {
-  Serial.begin(115200);
+  // Serial.begin(115200);
 
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT_PULLUP);
@@ -36,9 +21,6 @@ void setup() {
 
 	dc_motor_1.setSpeed(SPEED);
   dc_motor_2.setSpeed(SPEED);
-
-	// initSD();
-  // initMP3Player();
 }
 
 void loop()
@@ -50,7 +32,8 @@ void loop()
 
   Serial.println("START GAME?");
   while(true) {
-    int reading = digitalRead(buttonPin);
+    int reading = analogRead(buttonPin);
+    // Serial.println(reading);
     if (reading == 0) {
       break;
     }
@@ -60,71 +43,39 @@ void loop()
 	dc_motor_1.run(FORWARD);
 	dc_motor_2.run(FORWARD);
 
+  int lastButtonState = 1;
 	while (true)
 	{
     int reading = digitalRead(buttonPin);
-    if (reading == 0) {
+    Serial.println(reading);
+    if (reading == 0 && lastButtonState == 0) {
       break;
     }
+    lastButtonState = reading;
+
 	  dc_motor_1.setSpeed(0);
 	  dc_motor_2.setSpeed(0);
 		servo.write(90);
 		delay(500);
-   int reading = digitalRead(buttonPin);
-    if (reading == 0) {
-      break;
-    }
+
     dc_motor_1.setSpeed(SPEED);
 	  dc_motor_2.setSpeed(SPEED);
 		delay(4000);
-   int reading = digitalRead(buttonPin);
-    if (reading == 0) {
+
+    reading = digitalRead(buttonPin);
+    Serial.println(reading);
+    if (reading == 0 && lastButtonState == 0) {
       break;
     }
+    lastButtonState = reading;
+
 	  dc_motor_1.setSpeed(0);
 	  dc_motor_2.setSpeed(0);
 		servo.write(-90);
 		delay(500);
-   int reading = digitalRead(buttonPin);
-    if (reading == 0) {
-      break;
-    }
+
     dc_motor_1.setSpeed(SPEED);
 	  dc_motor_2.setSpeed(SPEED);
 		delay(4000);
 	}
-  dc_motor_1.run(RELEASE);
-  dc_motor_2.run(RELEASE);
-  delay(10000);
-}
-
-void initSD() {
-  // Initialize the SD card.
-  if(!sd.begin(SD_SEL, SPI_FULL_SPEED)) sd.initErrorHalt();
-  // Depending upon your SD card environment, SPI_HAVE_SPEED may work better.
-  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
-}
-
-void initMP3Player() {
-  uint8_t result = MP3player.begin();
-  if(result != 0) {
-    Serial.print("Error code: ");
-    Serial.print(result);
-    Serial.println(" when trying to start MP3 player");
-  }
-  MP3player.setVolume(volume, volume);
-}
-
-void playTrack(uint8_t track)
-{
-  uint8_t result = MP3player.playTrack(track);
-
-  if(result != 0) {
-    Serial.print("Error code: ");
-    Serial.print(result);
-    Serial.println(" when trying to play track");
-  } else {
-    Serial.print("Playing track: ");
-    Serial.println(track);
-  }
 }
